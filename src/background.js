871e85460891345c64caafc8e4d5220d1bbb90d8
daveavi <avidave97@gmail.com>
2020-08-03 17:47:09 -0400

    able to communicate between content and background scripts

--- src/background.js
+++ src/background.js
@@ -1,13 +1,24 @@
-chrome.browserAction.onClicked.addListener(buttonClicked)
 
-function buttonClicked(tab){
-  console.log(tab);
-  let msg = {
-    txt: "focus"
-  }
-  chrome.tabs.sendMessage(tab.id, msg)
-}
+var focus = true;
+var port;
+chrome.runtime.onConnect.addListener(function(connectionPort){
+    console.log("I am connected");
+    console.assert(connectionPort.name == "TwitterFocus"); 
+    port = connectionPort;
+    if (focus)
+      console.log("I am here");
+      port.postMessage({status: "focus"});
 
+    focus = !focus;
+});
 
+chrome.browserAction.onClicked.addListener(function(){
+    if (!focus)
+      port.postMessage({status: "focus"});
+    else
+      port.postMessage({status: "un-focus"});
+    
+    focus = !focus;
+});
 
-console.log("running background")
+console.log("running background script");
--- src/hide-feed-panel.js
+++ src/hide-feed-panel.js
@@ -1,9 +1,28 @@
+console.log("content script is good to go");
+
+var panelClassName = "css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x"
+var feedClassName = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu"
+var port = chrome.runtime.connect({name: "TwitterFocus"});
+
+port.onMessage.addListener(function(msg) {
+    console.log(msg.status);
+    if(msg.status == "focus")
+        setContentVisibility(false);
+    else
+        setContentVisibility(true);
+});
 
 
-chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(message, sender, sendResponse){
-    console.log(message.txt);
+
+function setContentVisibility(visiblity){
+    if(visiblity){
+        document.getElementsByClassName("css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x")[0].style.visibility = "visible";
+        document.getElementsByClassName("css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu")[1].style.visibility = "visible";
+    }else{
+        document.getElementsByClassName("css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x")[0].style.visibility = "hidden"
+        document.getElementsByClassName("css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu")[1].style.visibility  = "hidden";
+    }
 }
 
 
