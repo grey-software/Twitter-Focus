a3f1cd789f9bd68bf4da56d194b55541a6b1ea3e
daveavi <avidave97@gmail.com>
2020-08-03 22:08:09 -0400

    have basic functionality of icon working

--- src/hide-feed-panel.js
+++ src/hide-feed-panel.js
@@ -7,7 +7,7 @@ var port = chrome.runtime.connect({name: "TwitterFocus"});
 port.onMessage.addListener(function(msg) {
     console.log(msg.status);
     if(msg.status == "focus")
-        setContentVisibility(false);
+        setContentVisibility(false)
     else
         setContentVisibility(true);
 });
@@ -15,8 +15,8 @@ port.onMessage.addListener(function(msg) {
 
 
 
-function setContentVisibility(visiblity){
-    if(visiblity){
+function setContentVisibility(makeVisible){
+    if(makeVisible){
         document.getElementsByClassName("css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x")[0].style.visibility = "visible";
         document.getElementsByClassName("css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu")[1].style.visibility = "visible";
     }else{
