53b8d24d417f5ebdd0ed86e7ce248c4259886725
daveavi <avidave97@gmail.com>
2020-08-06 22:51:00 -0400

    added quotes to a different file

--- src/hide-feed-panel.js
+++ src/hide-feed-panel.js
@@ -2,7 +2,7 @@ console.log("content script is good to go");
 
 const panelClassName = "css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x"
 const feedClassName = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu"
-var port = chrome.runtime.connect({name: "TwitterFocus"});
+const port = chrome.runtime.connect({name: "TwitterFocus"});
 
 
 port.onMessage.addListener(function(msg) {     
@@ -41,7 +41,6 @@ function blockFeedPanel(){
         intervalId = setInterval(tryBlockingFeedPanel, 1000)
     else
         setContentVisibility(false)
-
 
 }
 
