789d05ff97ba8ac8887badefb93eca9b8e6537e7
daveavi <avidave97@gmail.com>
2020-08-10 19:17:57 -0400

    added twitter focus icon

--- src/hide-feed-panel.js
+++ src/hide-feed-panel.js
@@ -3,6 +3,7 @@ const FEED_CONTAINER_CLASS_NAME = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu"
 const FEED_LABEL = '[aria-label="Timeline: Your Home Timeline"]'
 const BOTTOM_LABEL = "css-1dbjc4n r-1niwhzg r-1tlfku8 r-1ylenci r-1phboty r-1yadl64 r-ku1wi2 r-1udh08x"
 
+const logoUrl = chrome.runtime.getURL("icon.png")
 const gsLogoUrl = chrome.runtime.getURL("logo.png")
 const tooninLogoUrl = chrome.runtime.getURL("toonin_logo.png")
 const materialMathLogoUrl = chrome.runtime.getURL("material_math_logo.png")
@@ -61,11 +62,10 @@ function blockFeedPanel () {
 function fillQuote () {
     var quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-    const hyperlinkStyle = "<style>a{text-decoration: none;color: black;} a:visited{text-decoration: none;color: black;} a:hover{text-decoration: none !important;opacity: 0.7;} </style>"
-    const gspanelTitleStyle = "style=\"color:#434343;font-size:24px;font-weight:700;text-align:center;margin-bottom:25px;\""
     const quoteStyle = "style=\"color:#293E4A;font-size:20px;\margin-bottom:4px;margin-left:10px;\""
     const tfTitleStyle = "style=\"color:#1DA1F2;font-size:28px;font-weight:700;margin-bottom:16px; margin-left:10px;\""
     const quoteSourceStyle = "style=\"color:#293E4A;font-size:20px;font-style:italic;margin-bottom:16px;margin-left:10px;\""
+    const logoStyle = " style=\"height: 24px;margin: 0px 4px;\" "
     const instructionStyle = "style=\"color:#293E4A;font-size:16px;\margin-bottom:4px;margin-left:10px;\""
     const instruction = "To exit focus mode, click on the Twitter Focus extension:"
 
@@ -74,6 +74,7 @@ function fillQuote () {
     linkedInFocusHTML += "<p " + quoteStyle + ">" + quote.text + "</p>"
     linkedInFocusHTML += "<p " + quoteSourceStyle + ">- " + quote.source + "</p>"
     linkedInFocusHTML += "<p " + instructionStyle + ">" + instruction
+    linkedInFocusHTML += "<img src=\"" + logoUrl + "\" " + logoStyle + ">" + " from the extensions panel on the top right corner of your screen.</p>"
 
     const quoteHtmlNode = document.createElement("div")
     quoteHtmlNode.innerHTML = linkedInFocusHTML
