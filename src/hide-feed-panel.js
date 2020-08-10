7fea19c87dd765f79de635761f91d6cfe5511185
daveavi <avidave97@gmail.com>
2020-08-10 15:44:26 -0400

    cleaned up some of the code

--- src/hide-feed-panel.js
+++ src/hide-feed-panel.js
@@ -1,5 +1,8 @@
-const panelClassName = "css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x"
-const feedClassName = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu"
+const PANEL_CLASS_NAME = "css-1dbjc4n r-1u4rsef r-9cbz99 r-1ylenci r-1phboty r-rs99b7 r-ku1wi2 r-1udh08x"
+const FEED_CONTAINER_CLASS_NAME = "css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu"
+const FEED_LABEL = '[aria-label="Timeline: Your Home Timeline"]'
+
+
 const port = chrome.runtime.connect({ name: "TwitterFocus" });
 
 
@@ -14,15 +17,15 @@ port.onMessage.addListener(function (msg) {
 
 function setContentVisibility (makeVisible) {
     if (makeVisible) {
-        document.getElementsByClassName(panelClassName)[0].style.visibility = "visible";
-        document.querySelector('[aria-label="Timeline: Your Home Timeline"]').style.visibility = "visible";
+        document.getElementsByClassName(PANEL_CLASS_NAME)[0].style.visibility = "visible";
+        document.querySelector(FEED_LABEL).style.visibility = "visible";
        
-        var quote = document.getElementsByClassName("css-1dbjc4n r-1jgb5lz r-1ye8kvj r-13qz1uu")[1].children[0] 
+        var quote = document.getElementsByClassName(FEED_CONTAINER_CLASS_NAME)[1].children[0] 
         quote.remove();
     
     } else {
-        document.getElementsByClassName(panelClassName)[0].style.visibility = "hidden";
-        document.querySelector('[aria-label="Timeline: Your Home Timeline"]').style.visibility = "hidden"
+        document.getElementsByClassName(PANEL_CLASS_NAME)[0].style.visibility = "hidden";
+        document.querySelector(FEED_LABEL).style.visibility = "hidden"
         fillQuote();
     }
 }
@@ -48,10 +51,10 @@ function blockFeedPanel () {
 function fillQuote () {
     var quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-    const quoteStyle = "style=\"color:#293E4A;font-size:24px;\margin-bottom:4px;margin-left:20px;\""
-    const tfTitleStyle = "style=\"color:#1DA1F2;font-size:32px;font-weight:700;margin-bottom:16px; margin-left:20px;\""
-    const quoteSourceStyle = "style=\"color:#293E4A;font-size:20px;font-style:italic;margin-bottom:16px;margin-left:20px;\""
-    const instructionStyle = "style=\"color:#293E4A;font-size:16px;\margin-bottom:4px;margin-left:20px;\""
+    const quoteStyle = "style=\"color:#293E4A;font-size:20px;\margin-bottom:4px;margin-left:10px;\""
+    const tfTitleStyle = "style=\"color:#1DA1F2;font-size:28px;font-weight:700;margin-bottom:16px; margin-left:10px;\""
+    const quoteSourceStyle = "style=\"color:#293E4A;font-size:20px;font-style:italic;margin-bottom:16px;margin-left:10px;\""
+    const instructionStyle = "style=\"color:#293E4A;font-size:16px;\margin-bottom:4px;margin-left:10px;\""
     const instruction = "To exit focus mode, click on the Twitter Focus extension:"
 
     var linkedInFocusHTML = "<h1 " + tfTitleStyle + ">Twitter Focus</h1>"
@@ -63,20 +66,20 @@ function fillQuote () {
     const quoteHtmlNode = document.createElement("div")
     quoteHtmlNode.innerHTML = linkedInFocusHTML
 
-    document.getElementsByClassName(feedClassName)[1].prepend(quoteHtmlNode)
-    document.getElementsByClassName(feedClassName)[1].style.fontFamily = "Arial, Helvetica";
+    document.getElementsByClassName(FEED_CONTAINER_CLASS_NAME)[1].prepend(quoteHtmlNode)
+    document.getElementsByClassName(FEED_CONTAINER_CLASS_NAME)[1].style.fontFamily = "Arial, Helvetica";
 
 }
 function isBlocked () {
     if (!hasLoaded()) {
         return false
     } else {
-        return document.getElementsByClassName(panelClassName)[0].style.visibility == "hidden"
+        return document.getElementsByClassName(PANEL_CLASS_NAME)[0].style.visibility == "hidden"
 
     }
 }
 function hasLoaded () {
-    return document.getElementsByClassName(panelClassName)[0] && document.getElementsByClassName(feedClassName)[1];
+    return document.getElementsByClassName(PANEL_CLASS_NAME)[0] && document.getElementsByClassName(FEED_CONTAINER_CLASS_NAME)[1];
 }
 
 
