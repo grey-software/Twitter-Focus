fa2bbae5c5d7b071481b96ea17e0d1f0cceec309
daveavi <avidave97@gmail.com>
2020-08-20 11:31:33 -0400

    added grey software donations card, however social icons take time to load

--- src/focus.js
+++ src/focus.js
@@ -9,7 +9,6 @@ const gsLogoUrl = chrome.runtime.getURL("logo.png")
 const paypalLogoUrl = chrome.runtime.getURL("paypal.png")
 
 const port = chrome.runtime.connect({ name: "TwitterFocus" });
-console.log(window.location.toString());
 port.postMessage({url:  window.location.toString()});
 
 var quoteFilled = false;
@@ -119,6 +118,14 @@ function fillQuote() {
     const logoStyle = " style=\"height: 24px;margin: 0px 4px;\" "
     const instructionStyle = "style=\"color:#293E4A;font-size:16px;\margin-bottom:4px;margin-left:10px;\""
     const instruction = "To exit focus mode, click on the Twitter Focus extension:"
+    const gsTitleStyle = "style=\"color:#434343;font-size:32px;font-weight:700;margin-right:auto;\""
+    const gsGithubStyle = "style=\"height: 32px;width: 32px;font-size: 32px;margin: 0px 6px;\""
+    const gsSocialLinkedInStyle = "style=\"background: #007bb5;color: white;height: 32px;width: 32px;font-size: 24px;margin: 0px 6px;padding: 6px;border-radius:4px;\""
+    const gsDesc = "This web extension was developed by Grey Software. Grey Software is a non-profit organization that aims to create the open source ecosystem of the future where software maintainers mentor students and build free software together!"
+    const hyperlinkStyle = "<style>a{text-decoration: none;color: black;} a:visited{text-decoration: none;color: black;} a:hover{text-decoration: none !important;opacity: 0.7;} </style>"
+    const paypalButtonStyle = "<style>.paypal-icon{height:24px;margin-right:4px}.paypal-button{border-radius:24px;height:42px;border:1px solid #003084;outline:none;display:flex;align-items:center;padding:2px 16px;color:#003084;font-size:18px;background-color:white;transition:all 0.3s ease-out}.paypal-button:hover{cursor:pointer;border:1px solid #1ba0de}.paypal-button:active{cursor:pointer;border:1px solid #1ba0de;color:white;background-color:#003084}</style>"
+    const gsCta = "To learn more, please visit the Grey Software website at "
+
 
     var linkedInFocusHTML = "<h1 " + tfTitleStyle + ">Twitter Focus</h1>"
     linkedInFocusHTML += "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"></link>"
@@ -126,6 +133,19 @@ function fillQuote() {
     linkedInFocusHTML += "<p " + quoteSourceStyle + ">- " + quote.source + "</p>"
     linkedInFocusHTML += "<p " + instructionStyle + ">" + instruction
     linkedInFocusHTML += "<img src=\"" + logoUrl + "\" " + logoStyle + ">" + " from the extensions panel on the top right corner of your screen.</p>"
+    linkedInFocusHTML += "<br>"
+    
+    linkedInFocusHTML += "<div style=\"border: 2px;border-style:solid;border-color:#434343;padding: 1em;width: 552px;height: 269px;margin-top: 20px;padding-top:2px;border-radius:4px;\">"
+    linkedInFocusHTML += "<div style=\"display: flex; align-items: center;margin-bottom:16px;\">"
+    linkedInFocusHTML += "<img src=\"" + gsLogoUrl + "\" style=\"height: 50px;float:left;margin-right: 6px;\" />"
+    linkedInFocusHTML += "<span " + gsTitleStyle + ">Grey Software</span>"
+    linkedInFocusHTML += "<a " + gsSocialLinkedInStyle + " href=\"https://www.linkedin.com/company/grey-software/\" class=\"fa fa-linkedin\"></a>"
+    linkedInFocusHTML += "<a " + gsGithubStyle + " href=\"https://github.com/grey-software\" class=\"fa fa-github\"></a>"
+    linkedInFocusHTML += "<a target=\"_blank\" href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VEAGAZP7DHJNE&source=url\"><button class=\"paypal-button\"><img class=\"paypal-icon\" src=\"https://assets.codepen.io/853141/paypal.png\"/>Donate</button></a></div>"
+    linkedInFocusHTML += hyperlinkStyle
+    linkedInFocusHTML += paypalButtonStyle
+    linkedInFocusHTML += "<div>" + gsDesc + "<p style=\"margin-top: 12px;\">" + gsCta + "</p><a href=\"https://org.grey.software/\">grey.software</a></div>"
+    linkedInFocusHTML += "</div>"
 
     const quoteHtmlNode = document.createElement("div")
     quoteHtmlNode.innerHTML = linkedInFocusHTML
