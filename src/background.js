432f669dfaf5024969cf065f66530cf82a839588
daveavi <avidave97@gmail.com>
2020-09-02 18:06:50 -0400

    added try catches around the functions in the background script

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
 var currentURL;
 var port;
 var focus = true;
+
 chrome.runtime.onConnect.addListener(function (connectionPort) {
   console.assert(connectionPort.name == "TwitterFocus");
   port = connectionPort;
+
   port.onMessage.addListener(function (msg) {
     sendStatus(msg.url, port);
     currentURL = msg.url
@@ -13,30 +15,63 @@ chrome.runtime.onConnect.addListener(function (connectionPort) {
 
 
 chrome.browserAction.onClicked.addListener(function () {
-  if ((!currentURL.includes("/explore") && !currentURL.includes("/messages")) && currentURL.includes("twitter.com")) {
-    if (!focus) {
-      if (currentURL == "https://twitter.com/home") {
-        port.postMessage({ status: "focus-home" });
+ 
+  try{
+    if ((!currentURL.includes("/explore") && !currentURL.includes("/messages")) && currentURL.includes("twitter.com")) {
+      if (!focus) {
+        if (currentURL == "https://twitter.com/home") {
+          port.postMessage({ status: "focus-home" });
+        } else {
+          port.postMessage({ status: "focus" })
+        }
       } else {
-        port.postMessage({ status: "focus" })
+        if (currentURL == "https://twitter.com/home") {
+          port.postMessage({ status: "unfocus-home" });
+        } else {
+          port.postMessage({ status: "unfocus" });
+        }
       }
-    } else {
-      if (currentURL == "https://twitter.com/home") {
-        port.postMessage({ status: "unfocus-home" });
-      } else {
-        port.postMessage({ status: "unfocus" });
-      }
+  
+      focus = !focus;
     }
 
-    focus = !focus;
+  }catch(err){
+    console.log("Looks like port or url is not defined")
   }
+  // if ((!currentURL.includes("/explore") && !currentURL.includes("/messages")) && currentURL.includes("twitter.com")) {
+  //   if (!focus) {
+  //     if (currentURL == "https://twitter.com/home") {
+  //       port.postMessage({ status: "focus-home" });
+  //     } else {
+  //       port.postMessage({ status: "focus" })
+  //     }
+  //   } else {
+  //     if (currentURL == "https://twitter.com/home") {
+  //       port.postMessage({ status: "unfocus-home" });
+  //     } else {
+  //       port.postMessage({ status: "unfocus" });
+  //     }
+  //   }
+
+  //   focus = !focus;
+  // }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.url && changeInfo.url.includes("twitter.com")) {
-    sendStatus(changeInfo.url, port);
-    currentURL = changeInfo.url
+  
+  try{
+    if (changeInfo.url && changeInfo.url.includes("twitter.com")) {
+      sendStatus(changeInfo.url, port);
+      currentURL = changeInfo.url
+    }
   }
+  catch(err){
+    console.log("Looks like port or url is not defined");
+  }
+  // if (changeInfo.url && changeInfo.url.includes("twitter.com")) {
+  //   sendStatus(changeInfo.url, port);
+  //   currentURL = changeInfo.url
+  // }
 
 });
 
